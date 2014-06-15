67c96a9862e1ff180766fef2f1fa912d1cd57421
spezi77 <spezialitz@gmx.de>
2014-06-15 11:12:36 +0200

    Clean up

--- config/common_full.mk
+++ config/common_full.mk
@@ -4,22 +4,5 @@ $(call inherit-product, vendor/carbon/config/common.mk)
 # LatinIME english dictionary
 $(call inherit-product, vendor/carbon/config/dictionaries/english.mk)
 
-# Bring in all audio files
-include frameworks/base/data/sounds/NewAudio.mk
-
-# Extra Ringtones
-include frameworks/base/data/sounds/AudioPackageNewWave.mk
-
 # Bring in Evervolv a2sd and init.d scripts
 include vendor/carbon/config/tools.mk
-
-# Optional Carbon packages
-PRODUCT_PACKAGES += \
-    HoloSpiralWallpaper \
-    MagicSmokeWallpapers \
-    NoiseField \
-    Galaxy4 \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    VisualizationWallpapers \
-    PhaseBeam
